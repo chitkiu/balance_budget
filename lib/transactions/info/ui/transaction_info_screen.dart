c060982568f3c3f40341a4653794d613c8f55de3
chitkiu <>
2023-02-18 02:02:29 +0200

    Added possibility to cancel transaction changes

--- lib/transactions/info/ui/transaction_info_screen.dart
+++ lib/transactions/info/ui/transaction_info_screen.dart
@@ -1,6 +1,5 @@
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
 import '../../../common/getx_extensions.dart';
@@ -9,69 +8,94 @@ import '../../../common/ui/common_ui_settings.dart';
 import '../../list/ui/models/transaction_ui_model.dart';
 import '../domain/transaction_info_controller.dart';
 
-class TransactionInfoScreen extends StatefulWidget {
+class TransactionInfoScreen extends GetView<TransactionInfoController> {
   final String transactionId;
 
-  const TransactionInfoScreen(this.transactionId, {Key? key}) : super(key: key);
+  TransactionInfoScreen(this.transactionId, {Key? key}) : super(key: key);
 
-  @override
-  State<TransactionInfoScreen> createState() => _TransactionInfoScreenState();
-}
-
-class _TransactionInfoScreenState extends State<TransactionInfoScreen> {
+  final RxBool _isInEditMode = false.obs;
 
   final TextEditingController _sumController = TextEditingController();
   final TextEditingController _commentController = TextEditingController();
 
-  TransactionInfoController get controller => Get.find();
-
-  bool _isInEditMode = false;
-
   @override
   Widget build(BuildContext context) {
     return StreamBuilder(
-      stream: controller.transactionById(widget.transactionId),
+      stream: controller.transactionById(transactionId),
       builder: (context, snapshot) {
         if (snapshot.hasData && snapshot.data != null) {
           var model = snapshot.data!;
+          //TODO Add cancel icon
           return PlatformScaffold(
             appBar: PlatformAppBar(
-              title: Text(Get.localisation.transactionInfoTitle),
+              title: Obx(() {
+                String title;
+                if (_isInEditMode.value) {
+                  title = Get.localisation.transactionInfoEditTitle;
+                } else {
+                  title = Get.localisation.transactionInfoTitle;
+                }
+                return Text(title);
+              }),
               trailingActions: [
                 if (model.canEdit)
                   GestureDetector(
-                    onTap: () => _changeEditMode(model),
-                    child: Icon(_isInEditMode ? CommonIcons.check : CommonIcons.edit),
-                  )
-              ],
-            ),
-            body: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                if (model is CommonTransactionUIModel)
-                  ..._commonWidgets(model),
-
-                if (model is SetBalanceTransactionUIModel)
-                  ..._setBalanceWidgets(model),
-
-                if (!_isInEditMode)
-                  PlatformTextButton(
-                    child: Text(Get.localisation.delete),
-                    onPressed: () async {
-                      await confirmBeforeActionDialog(
-                            () async {
-                          await controller.deleteTransaction(model.id);
-                          Get.back();
-                        },
-                        title: Get.localisation.confirmToDeleteTitle,
-                        subTitle: Get.localisation.confirmToDeleteText,
-                        confirmAction: Get.localisation.yes,
-                        cancelAction: Get.localisation.no,
-                      );
+                    onTap: () {
+                      _updateOnChangeMode(model, _isInEditMode.value);
+                      _isInEditMode.value = !_isInEditMode.value;
                     },
+                    child: Obx(() {
+                      return Icon(
+                          _isInEditMode.value ? CommonIcons.check : CommonIcons.edit);
+                    }),
                   )
               ],
+              automaticallyImplyLeading: false,
+              leading: Obx(() {
+                if (_isInEditMode.value) {
+                  return GestureDetector(
+                    onTap: () {
+                      _updateControllers(model, false);
+                      _isInEditMode.value = false;
+                    },
+                    child: Icon(Icons.cancel), //TODO Add platform icon
+                  );
+                } else {
+                  return GestureDetector(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: Icon(Icons.arrow_back), //TODO Add platform icon
+                  );
+                }
+              }),
             ),
+            body: Obx(() {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  if (model is CommonTransactionUIModel)
+                    ..._commonWidgets(model, _isInEditMode.value),
+                  if (model is SetBalanceTransactionUIModel) ..._setBalanceWidgets(model),
+                  if (!_isInEditMode.value)
+                    PlatformTextButton(
+                      child: Text(Get.localisation.delete),
+                      onPressed: () async {
+                        await confirmBeforeActionDialog(
+                          () async {
+                            await controller.deleteTransaction(model.id);
+                            Get.back();
+                          },
+                          title: Get.localisation.confirmToDeleteTitle,
+                          subTitle: Get.localisation.confirmToDeleteText,
+                          confirmAction: Get.localisation.yes,
+                          cancelAction: Get.localisation.no,
+                        );
+                      },
+                    )
+                ],
+              );
+            }),
           );
         } else {
           //TODO Add loader
@@ -81,55 +105,60 @@ class _TransactionInfoScreenState extends State<TransactionInfoScreen> {
     );
   }
 
-  void _changeEditMode(TransactionUIModel model) {
-    if (model.canEdit) {
-      if (_isInEditMode) {
-        controller.editTransaction(
-            model.id,
-            newSum: _sumController.text,
-            newComment: _commentController.text,
-        );
-        _sumController.clear();
-        _commentController.clear();
-      } else {
-        _sumController.text = model.sum;
-        _commentController.text = model.comment ?? '';
-      }
-      setState(() {
-        _isInEditMode = !_isInEditMode;
-      });
+  void _editTransaction(TransactionUIModel model) {
+    controller.editTransaction(
+      model.id,
+      newSum: _sumController.text,
+      newComment: _commentController.text,
+    );
+  }
+
+  void _updateControllers(TransactionUIModel model, bool isInEditMode) {
+    if (isInEditMode) {
+      _sumController.clear();
+      _commentController.clear();
+    } else {
+      _sumController.text = model.sum;
+      _commentController.text = model.comment ?? '';
     }
   }
 
-  List<Widget> _commonWidgets(CommonTransactionUIModel model) {
+  void _updateOnChangeMode(TransactionUIModel model, bool isInEditMode) {
+    if (model.canEdit) {
+      if (isInEditMode) {
+        _editTransaction(model);
+      }
+      _updateControllers(model, isInEditMode);
+    }
+  }
+
+  List<Widget> _commonWidgets(CommonTransactionUIModel model, bool isInEditMode) {
     return [
       Text(Get.localisation.transactionInfoSumPrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
-      if (!_isInEditMode)
-        Text(model.sum, style: TextStyle(color: model.sumColor),),
-      if (_isInEditMode)
+      if (!isInEditMode)
+        Text(
+          model.sum,
+          style: TextStyle(color: model.sumColor),
+        ),
+      if (isInEditMode)
         PlatformTextField(
           controller: _sumController,
         ),
-
       Text(Get.localisation.transactionInfoCategoryPrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.categoryName),
-
       Text(Get.localisation.transactionInfoAccountPrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.accountName),
-
       Text(Get.localisation.transactionInfoTimePrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.time),
-
-      if (model.comment != null || _isInEditMode)
+      if (model.comment != null || isInEditMode)
         Text(Get.localisation.transactionInfoCommentPrefix,
             style: const TextStyle(fontWeight: FontWeight.w500)),
-      if (model.comment != null && !_isInEditMode)
-        Text(model.comment ?? ''),
-      if (_isInEditMode)
+      if (model.comment != null && !isInEditMode) Text(model.comment ?? ''),
+      if (isInEditMode)
         PlatformTextField(
           controller: _commentController,
         ),
@@ -141,15 +170,12 @@ class _TransactionInfoScreenState extends State<TransactionInfoScreen> {
       Text(Get.localisation.transactionInfoSumPrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.sum),
-
       Text(Get.localisation.transactionInfoAccountPrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.accountName),
-
       Text(Get.localisation.transactionInfoTimePrefix,
           style: const TextStyle(fontWeight: FontWeight.w500)),
       Text(model.time),
     ];
   }
-
 }
