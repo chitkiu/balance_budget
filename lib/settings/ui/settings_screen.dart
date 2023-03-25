eee9900437d53c84d098a10352787d9bd68b1f9c
chitkiu <>
2023-03-26 01:14:15 +0200

    Added dialog before sign out

--- lib/settings/ui/settings_screen.dart
+++ lib/settings/ui/settings_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:get/get.dart';
 
 import '../../common/getx_extensions.dart';
+import '../../common/ui/common_ui_settings.dart';
 import '../domain/settings_controller.dart';
 
 class SettingsScreen extends GetView<SettingsController> {
@@ -27,7 +28,22 @@ class SettingsScreen extends GetView<SettingsController> {
           onPressed: controller.onManageCategoriesClick,
           child: Text(Get.localisation.manageCategoriesButtonText),
         ),
-        const SignOutButton(),
+        PlatformElevatedButton(
+          child: Text(Get.localisation.sign_out),
+          onPressed: () async {
+            await confirmBeforeActionDialog(
+              () async {
+                FirebaseUIAuth.signOut(
+                  context: context,
+                );
+              },
+              title: Get.localisation.sign_out,
+              subTitle: Get.localisation.sign_out_confirmation_subtitle,
+              confirmAction: Get.localisation.yes,
+              cancelAction: Get.localisation.no,
+            );
+          },
+        ),
       ],
     );
   }
